d98d40111adce1084d8ecfff2ae951d35a13012b
Ezz Abousaif <ezzeldin.abousaif@powerhrg.com>
2019-05-08 13:03:56 -0400

    Done.

---
+++ app/controllers/figures_controller.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+class FiguresController < ApplicationController
+  get "/figures" do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
+  get "/figures/new" do
+    @landmarks = Landmark.all
+    erb :'/figures/new'
+  end
+
+  post "/figures" do
+    @figure = Figure.create(params[:figure])
+    unless params[:title][:name].empty?
+      @new_title = Title.create(params[:title])
+      @figure.titles << @new_title
+    end
+
+    unless params[:landmark][:name].empty?
+      @new_landmark = Landmark.create(params[:landmark])
+      @figure.landmarks << @new_landmark
+    end
+
+    redirect "/figures/#{@figure.id}"
+  end
+
+  get "/figures/:id" do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+  end
+
+  get "/figures/:id/edit" do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/edit'
+  end
+
+  patch "/figures/:id" do
+    @figure = Figure.find_by_id(params[:id])
+    @figure.name = params[:figure][:name]
+    @figure.title_ids = params[:title_ids]
+    @figure.landmark_ids = params[:landmarks_ids]
+
+    unless params[:title][:name].empty?
+      @new_title = Title.create(params[:title])
+      @figure.titles << @new_title
+      @figure.save
+    end
+
+    unless params[:landmark][:name].empty?
+      @new_landmark = Landmark.create(params[:landmark])
+      @figure.landmarks << @new_landmark
+      @figure.save
+    end
+    @figure.save
+    redirect "/figures/#{@figure.id}"
+  end
+end
---
+++ db/migrate/20190508122958_create_landmarks.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class CreateLandmarks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :landmarks do |t|
+      t.string :name
+      t.integer :figure_id
+      t.integer :year_completed
+    end
+  end
+end
